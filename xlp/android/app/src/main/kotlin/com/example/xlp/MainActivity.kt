fdb42d262b2303efeab989e578467bfec311bc41
juronghuxue <2414619849@qq.com>
2020-04-27 23:22:05 +0800

    day03-1

---
+++ xlp/android/app/src/main/kotlin/com/example/xlp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xlp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xlp/lib/main.dart
@@ -0,0 +1,101 @@
+
+import 'package:flutter/material.dart';
+import './demo/listview_demo.dart';
+import './demo/hello_demo.dart';
+void main() => runApp(App());
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Home(),
+      theme:ThemeData(
+        primarySwatch: Colors.yellow,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70
+      )
+    );
+  }
+}
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length:3,
+      child:Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading:IconButton(
+            icon:Icon(Icons.menu),
+            tooltip: 'Navigration',
+            onPressed: ()=>debugPrint('Navigration button is pressed.'),
+          ),
+          title:Center(
+            child: Text(
+              'HIHA11'
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon:Icon(Icons.search),
+              tooltip:'Search',
+              onPressed: ()=>debugPrint('Search button is pressed.'),
+            ),
+          ],
+          elevation: 0.0,
+          bottom:TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(icon:Icon(Icons.local_florist)),
+              Tab(icon:Icon(Icons.change_history)),
+              Tab(icon:Icon(Icons.directions_bike)),
+            ],
+          )
+        ),
+        body:TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist,size:128.0,color:Colors.black12),
+            Icon(Icons.change_history,size:128.0,color:Colors.black12),
+            Icon(Icons.directions_bike,size:128.0,color:Colors.black12),
+          ],
+        ),
+        drawer:Drawer(
+          child:ListView(
+            padding:EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child:Text('header'.toUpperCase()),
+                decoration: BoxDecoration(
+                  color:Colors.grey[100],
+                ),
+              ),
+              ListTile(
+                title:Text('Messages:',textAlign:TextAlign.right),
+                trailing: Icon(Icons.message,color:Colors.black12,size:22.0),
+              ),
+              ListTile(
+                title:Text('Favorite:',textAlign:TextAlign.right),
+                trailing: Icon(Icons.favorite,color:Colors.black12,size:22.0),
+              ),
+              ListTile(
+                title:Text('Settings:',textAlign:TextAlign.right),
+                trailing: Icon(Icons.settings,color:Colors.black12,size:22.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
