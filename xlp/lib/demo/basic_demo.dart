af5b762fa8a21f17d50342b87f679a3a53b57dd1
juronghuxue <2414619849@qq.com>
2020-04-29 23:25:15 +0800

    day05-0ver

--- xlp/lib/demo/basic_demo.dart
+++ xlp/lib/demo/basic_demo.dart
@@ -5,16 +5,61 @@ class BasicDemo extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      color:Colors.grey[100],
+      // color:Colors.grey[100],
+      decoration: BoxDecoration(
+        image:DecorationImage(
+          image:NetworkImage('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588174677747&di=171ebd8787e2447d74d0f36132796df3&imgtype=0&src=http%3A%2F%2Fa2.att.hudong.com%2F36%2F48%2F19300001357258133412489354717.jpg'),
+          alignment: Alignment.topCenter,
+          // repeat: ImageRepeat.repeatY,
+          fit:BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.indigoAccent[400].withOpacity(0.5),
+            BlendMode.hardLight,
+          )
+        ),
+      ),
       child:Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Container(
             child:Icon(Icons.pool,size:32.0,color:Colors.white),
-            color:Color.fromRGBO(3, 54, 255, 1.0),
+            
             padding:EdgeInsets.all(16.0),
             margin:EdgeInsets.all(8.0),
             width:90.0,
-            height:90.0
+            height:90.0,
+            decoration: BoxDecoration(
+              color:Color.fromRGBO(3,54,255,1.0),
+              border:Border.all(
+                color:Colors.indigoAccent[100],
+                width:3.0,
+                style:BorderStyle.solid,
+              ),
+              // borderRadius:BorderRadius.circular(16.0),
+              boxShadow: [
+                BoxShadow(
+                  offset:Offset(0.0, 16.0),
+                  color:Color.fromRGBO(16, 20, 188, 1.0),
+                  blurRadius: 25.0,
+                  spreadRadius: -9.0
+                )
+              ],
+              shape:BoxShape.circle,
+              // gradient: RadialGradient(
+              //   colors:[
+              //     Color.fromRGBO(7,102,255,1.0),
+              //     Color.fromRGBO(3,28,128,1.0),
+              //   ]
+              // ),
+              gradient: LinearGradient(
+                colors:[
+                  Color.fromRGBO(7,102,255,1.0),
+                  Color.fromRGBO(3,28,128,1.0),
+                ],
+                begin: Alignment.topCenter,
+                end:AlignmentDirectional.bottomCenter,
+              )
+            ),
           )
         ],
       ),
--- xlp/lib/main.dart
+++ xlp/lib/main.dart
@@ -5,6 +5,7 @@ import './demo/hello_demo.dart';
 import './demo/drawer_demo.dart';
 import './demo/bottom_navigation_bar_demo.dart';
 import './demo/basic_demo.dart';
+import './demo/layout_demo.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget{
@@ -61,7 +62,8 @@ class Home extends StatelessWidget{
             ListViewDemo(),
             // Icon(Icons.change_history,size:128.0,color:Colors.black12),
             BasicDemo(),
-            Icon(Icons.directions_bike,size:128.0,color:Colors.black12),
+            // Icon(Icons.directions_bike,size:128.0,color:Colors.black12),
+            LayoutDemo(),
           ],
         ),
         drawer:DrawerDemo(),
